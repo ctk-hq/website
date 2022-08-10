bf6975d3dc83e75ceccd93a928ab5d4bc5094704
Samuel Rowe <samuelrowe1999@gmail.com>
2022-08-10 17:42:16 +0530

    feat: created custom app component

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
-import '../styles/globals.css'
+import { useEffect } from "react";
+
+import "../styles/globals.css";
+import "../components/css/style.css";
+// import "tailwindcss/tailwind.css";
+import "aos/dist/aos.css";
+
+import AOS from "aos";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    AOS.init({
+      once: true,
+      disable: "phone",
+      duration: 700,
+      easing: "ease-out-cubic",
+    });
+  });
+
+  useEffect(() => {
+    document.querySelector("html").style.scrollBehavior = "auto";
+    window.scroll({ top: 0 });
+    document.querySelector("html").style.scrollBehavior = "";
+  }, []);
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
